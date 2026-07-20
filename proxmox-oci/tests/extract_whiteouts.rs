723687ea2fd326f271f05ed866278a4dab4bca9d
Filip Schauer <f.schauer@proxmox.com>
2026-07-20 11:50:19 +0200

    oci: tests: return Result from tests to avoid unwrap calls
    Signed-off-by: Filip Schauer <f.schauer@proxmox.com>

--- proxmox-oci/tests/extract_whiteouts.rs
+++ proxmox-oci/tests/extract_whiteouts.rs
@@ -1,92 +1,99 @@
 use std::fs::remove_dir_all;
 
+use anyhow::Result;
+
 use proxmox_oci::{Arch, parse_and_extract_image};
 use proxmox_sys::fs::make_tmp_dir;
 
 #[test]
-fn test_whiteout_root_breakout() {
-    let extract_dir = make_tmp_dir("/tmp/", None).unwrap();
+fn test_whiteout_root_breakout() -> Result<()> {
+    let extract_dir = make_tmp_dir("/tmp/", None)?;
 
     parse_and_extract_image(
         &"tests/oci_image_data/oci_test_whiteout_root_breakout.tar".into(),
         &extract_dir,
         Some(&Arch::Amd64),
-    )
-    .unwrap();
+    )?;
 
     // Check that the whiteout did not remove the root directory
     assert!(extract_dir.exists());
 
     // Cleanup
-    remove_dir_all(extract_dir).unwrap();
+    remove_dir_all(extract_dir)?;
+
+    Ok(())
 }
 
 #[test]
-fn test_whiteout_root_parent_breakout() {
-    let extract_dir = make_tmp_dir("/tmp/", None).unwrap();
+fn test_whiteout_root_parent_breakout() -> Result<()> {
+    let extract_dir = make_tmp_dir("/tmp/", None)?;
 
     parse_and_extract_image(
         &"tests/oci_image_data/oci_test_whiteout_root_parent_breakout.tar".into(),
         &extract_dir,
         Some(&Arch::Amd64),
-    )
-    .unwrap();
+    )?;
 
     // Check that the whiteout did not remove the root directory
     assert!(extract_dir.exists());
 
     // Cleanup
-    remove_dir_all(extract_dir).unwrap();
+    remove_dir_all(extract_dir)?;
+
+    Ok(())
 }
 
 #[test]
-fn test_whiteout_current_directory() {
-    let extract_dir = make_tmp_dir("/tmp/", None).unwrap();
+fn test_whiteout_current_directory() -> Result<()> {
+    let extract_dir = make_tmp_dir("/tmp/", None)?;
 
     parse_and_extract_image(
         &"tests/oci_image_data/oci_test_whiteout_current_directory.tar".into(),
         &extract_dir,
         Some(&Arch::Amd64),
-    )
-    .unwrap();
+    )?;
 
     assert!(!extract_dir.join("etc").exists());
 
     // Cleanup
-    remove_dir_all(extract_dir).unwrap();
+    remove_dir_all(extract_dir)?;
+
+    Ok(())
 }
 
 #[test]
-fn test_whiteout_symlink() {
-    let extract_dir = make_tmp_dir("/tmp/", None).unwrap();
+fn test_whiteout_symlink() -> Result<()> {
+    let extract_dir = make_tmp_dir("/tmp/", None)?;
 
     parse_and_extract_image(
         &"tests/oci_image_data/oci_test_whiteout_symlink.tar".into(),
         &extract_dir,
         Some(&Arch::Amd64),
-    )
-    .unwrap();
+    )?;
 
     assert!(extract_dir.join("etc/passwd").exists());
     assert!(!extract_dir.join("localetc").exists());
 
     // Cleanup
-    remove_dir_all(extract_dir).unwrap();
+    remove_dir_all(extract_dir)?;
+
+    Ok(())
 }
 
 #[test]
-fn test_whiteout_dead_symlink_parent() {
-    let extract_dir = make_tmp_dir("/tmp/", None).unwrap();
+fn test_whiteout_dead_symlink_parent() -> Result<()> {
+    let extract_dir = make_tmp_dir("/tmp/", None)?;
 
     parse_and_extract_image(
         &"tests/oci_image_data/oci_test_whiteout_dead_symlink_parent.tar".into(),
         &extract_dir,
         Some(&Arch::Amd64),
-    )
-    .unwrap();
+    )?;
 
     assert!(extract_dir.join("etc/passwd").exists());
 
     // Cleanup
-    remove_dir_all(extract_dir).unwrap();
+    remove_dir_all(extract_dir)?;
+
+    Ok(())
 }
